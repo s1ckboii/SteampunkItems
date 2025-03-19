062026fc23b91bb827bb9f4e7e8a27ccbe7493fd
s1ckboy <sickxedits@gmail.com>
2025-03-19 18:09:16 +0100

    #

---
+++ Headset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.Events;
+
+namespace SteampunkItems
+{
+    public class Headset : MonoBehaviour
+    {
+        public UnityEvent playRandomMusic;
+
+        private ItemToggle toggle;
+
+        private void Start ()
+        {
+            toggle = GetComponent<ItemToggle>();
+        }
+    }
+}
--- Plugins.cs
+++ Plugins.cs
@@ -1,8 +1,6 @@
 ﻿using System.IO;
 using UnityEngine;
 using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
 using REPOLib.Modules;
 using System.Collections.Generic;
 
@@ -10,15 +8,8 @@ namespace SteampunkItems
 {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     [BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
-    public class Plugin : BaseUnityPlugin
+    public class Plugins : BaseUnityPlugin
     {
-        public static Plugin Instance { get => instance; private set => instance = value; }
-        private static Plugin instance = null!;
-        internal static new ManualLogSource Logger = null!;
-        private Harmony harmony;
-
-        internal Harmony Harmony { get => harmony; set => harmony = value; }
-
         private void Awake()
         {
             string pluginFolderPath = Path.GetDirectoryName(Info.Location);
@@ -27,10 +18,12 @@ namespace SteampunkItems
             GameObject val1 = assetBundle.LoadAsset<GameObject>("Valuable MagniGlass_SP");
             GameObject val2 = assetBundle.LoadAsset<GameObject>("Valuable Telescope_SP");
             GameObject val3 = assetBundle.LoadAsset<GameObject>("Valuable Stopwatch_SP");
+            GameObject val4 = assetBundle.LoadAsset<GameObject>("Valuable HeadSet_SP");
             List<string> list = ["Valuables - Generic"];
             Valuables.RegisterValuable(val1, list);
             Valuables.RegisterValuable(val2, list);
             Valuables.RegisterValuable(val3, list);
+            Valuables.RegisterValuable(val4, list);
             GameObject item1 = assetBundle.LoadAsset<GameObject>("Item Melee Pickaxe_SP");
             Item item = assetBundle.LoadAsset<Item>("Item Melee Pickaxe_SP");
             Items.RegisterItem(item);
