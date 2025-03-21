693c2d9bf49b019aeee7489940955c86484c400b
s1ckboy <sickxedits@gmail.com>
2025-03-21 05:26:15 +0100

    ;

--- Plugins.cs
+++ Plugins.cs
@@ -4,43 +4,42 @@ using BepInEx;
 using REPOLib.Modules;
 using System.Collections.Generic;
 
-namespace SteampunkItems
+namespace SteampunkItems;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugins : BaseUnityPlugin
 {
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    [BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
-    public class Plugins : BaseUnityPlugin
+    private void Awake()
     {
-        private void Awake()
-        {
-            string pluginFolderPath = Path.GetDirectoryName(Info.Location);
-            string assetBundleFilePath = Path.Combine(pluginFolderPath, "steampunkitems");
-            AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundleFilePath);
+        string pluginFolderPath = Path.GetDirectoryName(Info.Location);
+        string assetBundleFilePath = Path.Combine(pluginFolderPath, "steampunkitems");
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundleFilePath);
 
-            if (assetBundle == null)
-            {
-                Logger.LogError("Failed to load SteampunkItems assetbundle.");
-                return;
-            }
-
-            List<string> valuabaleAssetNames = 
-            [
-                "Valuable MagniGlass_SP",
-                "Valuable Telescope_SP",
-                "Valuable Stopwatch_SP",
-                "Valuable HeadSet_SP"
-            ];
-            List<string> genericList = ["Valuables - Generic"];
-            foreach (var assetNames in valuabaleAssetNames)
-            {
-                RegisterValuable(assetBundle, assetNames, genericList);
-            }
-            Item meleePickaxe = assetBundle.LoadAsset<Item>("Item Melee Pickaxe_SP");
-            Items.RegisterItem(meleePickaxe);
-        }
-        private void RegisterValuable(AssetBundle assetBundle, string assetName, List<string> list)
+        if (assetBundle == null)
         {
-            GameObject valuables = assetBundle.LoadAsset<GameObject>(assetName);
-            Valuables.RegisterValuable(valuables, list);
+            Logger.LogError("Failed to load SteampunkItems assetbundle.");
+            return;
         }
+
+        List<string> valuabaleAssetNames = 
+        [
+            "Valuable MagniGlass_SP",
+            "Valuable Telescope_SP",
+            "Valuable Stopwatch_SP",
+            "Valuable HeadSet_SP"
+        ];
+        List<string> genericList = ["Valuables - Generic"];
+        foreach (var assetNames in valuabaleAssetNames)
+        {
+            RegisterValuable(assetBundle, assetNames, genericList);
+        }
+        Item meleePickaxe = assetBundle.LoadAsset<Item>("Item Melee Pickaxe_SP");
+        Items.RegisterItem(meleePickaxe);
+    }
+    private void RegisterValuable(AssetBundle assetBundle, string assetName, List<string> list)
+    {
+        GameObject valuables = assetBundle.LoadAsset<GameObject>(assetName);
+        Valuables.RegisterValuable(valuables, list);
     }
 }
